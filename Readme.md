7252c870d711a65614fd57d3c3fc20b4007c9015
Philipp Hochmann <phil.hochmann@gmail.com>
2022-04-25 16:27:36 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# DBIS Relational Model 
+
+This set of classes is used to define objects of the relational model (as a result of a conversion from an ER-diagram)
+
+## Usage
+
+Todo
---
+++ tests/test_Task.py
@@ -0,0 +1,13 @@
+import json
+from locale import ERA
+from tests.basetest import Basetest
+from excmanager.Task import *
+
+class TestRelational(Basetest):
+    def setUp(self, debug=False, profile=True):
+        return super().setUp(debug, profile)
+
+    def testExc(self):
+        '''
+            Todo
+        '''
